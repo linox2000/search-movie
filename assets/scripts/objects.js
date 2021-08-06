144d80e84e8facba747a8c3070b3026a1d1a658d
linox2000 <mllino1@hotmail.com>
2021-08-06 11:42:38 +0100

    Finished renderMovie function

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -1,21 +1,38 @@
-const addMovieBtn = document.getElementById('add-movie-btn');
+const addMovieBtn = document.getElementById("add-movie-btn");
 
 const movies = [];
-const addMovieHaandler = () =>{
-  const title = document.getElementById('title').value;
-  const extraName = document.getElementById('extra-name').value;
-  const extraValue = document.getElementById('extra-value').value;
+const addMovieHaandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
 
+  if (title === "" || extraName === "" || extraValue === "") return;
   const newMovies = {
-    info:{
+    info: {
       title,
-      [extraName]:extraValue
-    }
-  }
+      [extraName]: extraValue,
+    },
+  };
   movies.push(newMovies);
-  console.log(movies)
+  renderMovie();
+};
 
+const renderMovie = () => {
+  const movieList = document.getElementById("movie-list");
 
-}
-
-addMovieBtn.addEventListener('click',addMovieHaandler)
+  if (movies.length === 0) return;
+  else movieList.classList.add("visible");
+  const newMovieEl = document.createElement("li");
+  movies.forEach((movie) => {
+    let text = movie.info.title + " - ";
+    for (const key in movie.info) {
+      if (key !== 'title') {
+        text = text + `${key} : ${movie.info[key]}`
+        
+      }
+    }
+    newMovieEl.textContent = text;
+    movieList.append(newMovieEl);
+  });
+};
+addMovieBtn.addEventListener("click", addMovieHaandler);
